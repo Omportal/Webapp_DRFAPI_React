a3e4fb254f36a1df3e076a06cd69de909bed2ee4
Slava <omportalmusic@gmail.com>
2022-07-24 12:35:02 +0300

    create API serialize items

--- backend/API/models.py
+++ backend/API/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Items(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='img/')
+
+    def __str__(self):
+        return self.title
--- backend/API/views.py
+++ backend/API/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Items
+from .serializers import ItemSerializer
+from rest_framework import generics
+
 
 # Create your views here.
+
+class ItemsList(generics.ListAPIView):
+    serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        return Items.objects.all()
